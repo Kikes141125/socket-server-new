67a4dfb227231e9e4e7e37c834c7ac546760e824
Victor <victor.mendozabezzolo@gmail.com>
2021-03-20 16:09:32 -0500

    Commit - rest Server New

---
+++ README.md
@@ -0,0 +1,22 @@
+# Socket-Server
+
+Reconstruir módulos de node
+```
+npm install
+
+```
+
+Generar del DIST
+
+```
+tsc -w
+
+```
+
+Levantar servidor
+
+```
+
+nodemon dist/
+
+```
---
+++ index.ts
@@ -0,0 +1,21 @@
+import Server from './classes/server';
+import router from './routes/router';
+import cors from 'cors';
+// import bodyParser from 'body-parser';
+import express from 'express';
+
+const server= new Server();
+
+
+//Passing body parser
+server.app.use( express.urlencoded ( {extended:true} ) );
+server.app.use(express.json());
+
+//CORS
+server.app.use( cors( { origin:true, credentials: true } ));
+
+server.app.use( '/', router )
+
+server.start( ()=>{
+    console.log(`Servidor corriendo en el puerto ${server.port}`);
+} )
